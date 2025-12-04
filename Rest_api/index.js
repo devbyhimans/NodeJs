9dddc23479c610c0e5e2e2367e62e146c9e9fa45
Himanshu <himanshu620434@gmail.com>
2025-12-05 01:56:21 +0530

    Implementation of PATCH,POST,DELETE  api Operations via POSTMAN

--- Rest_api/index.js
+++ Rest_api/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 
 const app=express();
 const PORT =8000;
+
+//Middleware->whenever we get form data this middleware puts that data into body.
+// 1. For parsing application/x-www-form-urlencoded (HTML Forms)
+app.use(express.urlencoded({ extended: false }));
+
+// 2. For parsing application/json (Postman / React / Axios)
+app.use(express.json());  //
 
 //rendering a HTML document to this path
 app.get('/users',(req,res)=>{
@@ -27,14 +35,66 @@ app.get('/api/users/:id',(req,res)=>{
  return res.json(user);
 });
 
+//add users-->get the user through POST using POSTMAN
 app.post('/api/users',(req,res)=>{
- //edit the user with id given
- return res.json({status: "pending"});
+    //body contains the data sent from the frontend
+    const body = req.body;
+    console.log("Body: ",body);
+
+    //adding data to the users
+    users.push({...body,id:users.length+1});
+    fs.writeFileSync("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+    return res.json("Data added successfully",body);
+    })
+});
+
+//edit the user with given id
+app.patch('/api/users',(req,res)=>{
+    //body contains the data sent from the frontend
+    const id = Number(req.body.id);
+    console.log(id);
+    const body =req.body;
+    const userIndex = users.findIndex((user)=>user.id == id);
+
+    if(userIndex==-1){
+      return res.status(404).json({status:"error",message:"user not found"});  
+    }
+    console.log("id: ",id);
+
+    // Update the user object
+    // We merge the existing user data (...users[userIndex]) 
+    // with the new data (...body). The body fields overwrite the old ones.
+    const updatedUser = { ...users[userIndex], ...body };
+    users[userIndex] = updatedUser;
+
+    // Writing the updated array back to the file
+    fs.writeFileSync("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+        if (err) {
+            return res.json({ status: "error", message: err });
+        }
+        return res.json({ status: "Success", user: updatedUser });
+    })
 });
 
 app.delete('/api/users/:id',(req,res)=>{
- //delete user with id given
-  return res.json({status: "pending"});
+    const id = Number(req.params.id);
+    console.log(id);
+
+    //delete user with id given
+    const userIndex = users.findIndex((user)=>user.id == id);
+    if(userIndex==-1){
+        return res.status(404).json({status:"error",message:"user not found"});  
+    }
+
+    // Remove the user from the array
+    // .splice(startIndex, deleteCount) -> removes 1 element at the found index
+    const deletedUser = users.splice(userIndex,1)[0];
+    // Get the first element (index 0) of the returned array,basically return the deleteduser to the client
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({ status: "Success", message: "User Deleted", deletedUser: deletedUser });
+    });
+
 });
 
 /*---modern way to write routes having same path--
